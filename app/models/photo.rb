1296ea245cd05c91180d0f2e446f881c2f8688e1
Chris Barnes <randomutterings@gmail.com>
2009-02-13 11:47:02 -0500

    added image previews with fade in and out using jquery

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :listing
   has_attached_file :photo, 
                     :styles => { :small => "100x100>",
-                                 :medium => "300x300>",
+                                 :medium => "400x400>",
                                  :large => "600x600>" }
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function addClickHandlers(){
+	
+	// Adds click handlers to image thumbnails that update the larger image
+	$("img.thumbnail", this).click(function() {
+		src = this.src.replace("small", "medium")
+		$("#medium_photo").fadeOut(200, function(){
+			$("#medium_photo").attr('src',src).bind('readystatechange load', function(){
+		   if (this.complete) $(this).fadeIn(400);
+			});
+		});
+	});
+	
+};
+
+$(document).ready(addClickHandlers);
