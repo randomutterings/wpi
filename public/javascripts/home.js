4174d66a33323a1d59c735e5e2cffb8722f52649
Chris Barnes <randomutterings@gmail.com>
2009-02-24 22:52:27 -0500

    fixed internet explorer rendering issues

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,12 +1,12 @@
 function cycleFeaturedProperties(hide, show){
 	// Cycles the featured properties on the home page
-	jQuery("#" + hide).fadeOut(200, function(){
+	jQuery("#feature" + hide).fadeOut(200, function(){
 		if (hide == jQuery(".feature").length){
 			hide = 1;
 		}else{
 			hide++;
 		}
-		jQuery("#" + show).fadeIn(200, function(){
+		jQuery("#feature" + show).fadeIn(200, function(){
 			if (show == jQuery(".feature").length) {
 				hide = show;
 				show = 1;
@@ -20,9 +20,7 @@ function cycleFeaturedProperties(hide, show){
 
 function showFirstProperty(){
 	if (jQuery(".feature")){
-		jQuery("#1").fadeIn(200, function(){
-			window.setTimeout(function(){ cycleFeaturedProperties(1, 2) }, 3000);
-		})
+		window.setTimeout(function(){ cycleFeaturedProperties(1, 2) }, 3000);
 	}
 }
 
