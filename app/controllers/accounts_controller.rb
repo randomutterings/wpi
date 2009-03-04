4f82a3252f624c1fc71fbfd1bff24f5a5981a10d
Chris Barnes <randomutterings@gmail.com>
2009-03-04 10:11:40 -0500

    created sessions and accounts with nifty_authentication

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,17 @@
+class AccountsController < ApplicationController
+  before_filter :authenticate, :except => [:new, :create]
+  def new
+    @account = Account.new
+  end
+  
+  def create
+    @account = Account.new(params[:account])
+    if @account.save
+      session[:account_id] = @account.id
+      flash[:notice] = "Thank you for signing up! You are now logged in."
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  before_filter :authenticate, :except => [:new, :create, :destroy]
+  def new
+  end
+  
+  def create
+    account = Account.authenticate(params[:login], params[:password])
+    if account
+      session[:account_id] = account.id
+      flash[:notice] = "Logged in successfully."
+      redirect_to root_url
+    else
+      flash.now[:error] = "Invalid login or password."
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    session[:account_id] = nil
+    flash[:notice] = "You have been logged out."
+    redirect_to root_url
+  end
+end
