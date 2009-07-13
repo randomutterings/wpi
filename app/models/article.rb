0138ebc779bef0bb1323b5888596a146d7be6b34
Chris Barnes <randomutterings@gmail.com>
2009-07-13 13:04:22 -0400

    updated images

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates_presence_of :title
   has_attached_file :background_image, 
-                    :styles => { :small => "518x100>", :original => "777x150>" }
+                    :styles => { :small => "518x100>", :original => "840x150>" }
 end
