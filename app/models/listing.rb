69d9d4e09984bb69f8a807a4004fa14dabf9042e
Chris Barnes <randomutterings@gmail.com>
2009-08-20 20:55:08 -0400

    fixed search query missing quotes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -16,12 +16,31 @@ class Listing < ActiveRecord::Base
     params[:min_price] = 0 if params[:min_price].blank?
     params[:max_price] = 9999999999 if params[:max_price].blank?
     conditions = "price > #{params[:min_price]} and price < #{params[:max_price]}"
-    conditions << " and city = #{params[:city]}" unless params[:city].blank?
-    conditions << " and state = #{params[:state]}" unless params[:state].blank?
-    conditions << " and zip = #{params[:zip]}" unless params[:zip].blank?
-    conditions << " and county = #{params[:county]}" unless params[:county].blank?
-    conditions << " and bedrooms = #{params[:bedrooms]}" unless params[:bedrooms].blank?
-    conditions << " and bathrooms = #{params[:bathrooms]}" unless params[:bathrooms].blank?
+    conditions << " and city = '#{params[:city]}'" unless params[:city].blank?
+    conditions << " and state = '#{params[:state]}'" unless params[:state].blank?
+    conditions << " and zip = '#{params[:zip]}'" unless params[:zip].blank?
+    conditions << " and county = '#{params[:county]}'" unless params[:county].blank?
+    conditions << " and bedrooms = '#{params[:bedrooms]}'" unless params[:bedrooms].blank?
+    conditions << " and bathrooms = '#{params[:bathrooms]}'" unless params[:bathrooms].blank?
+    listings = self.all(:conditions => conditions)
+  end
+  
+  def self.search(params)
+    params[:min_price] = 0 if params[:min_price].blank?
+    if params[:max_price].blank?
+      params[:max_price] = 9999999999
+    else
+      params[:max_price] = params[:max_price].to_i + 1
+      params[:max_price] = params[:max_price].to_s
+    end
+    conditions = Hash.new(true)
+    conditions["price"] = params[:min_price]...params[:max_price]
+    conditions["city"] = params[:city] unless params[:city].blank?
+    conditions["state"] = params[:state] unless params[:state].blank?
+    conditions["zip"] = params[:zip] unless params[:zip].blank?
+    conditions["county"] = params[:county] unless params[:county].blank?
+    conditions["bedrooms"] = params[:bedrooms] unless params[:bedrooms].blank?
+    conditions["bathrooms"] = params[:bathrooms] unless params[:bathrooms].blank?
     listings = self.all(:conditions => conditions)
   end
   
